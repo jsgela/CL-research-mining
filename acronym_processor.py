7cb903265d3e7d2c04ae17c0b0564c68109f1a01
Di5to <di5tortion@yahoo.com>
2018-12-18 01:54:51 -0500

    create an acronym dictionary

--- acronym_processor.py
+++ acronym_processor.py
@@ -1,33 +1,66 @@
-from nltk import sent_tokenize, word_tokenize
-import Corpus
+import nltk
+from nltk.corpus import words, names, stopwords
+from nltk.util import ngrams
 from Corpus import Corpus
 import re
-from nltk.corpus import words, names
+
 
 def get_acronyms(ACL_corpus):
 
+    #create a full list of tokenized words from the corpus
     full_text = []
-
     for thing in ACL_corpus.documents():
         for lsts in ACL_corpus.tokenized_sentences_in_file(thing):
             full_text.extend(lsts)
 
+    # print(filtered_text)
+
     r = re.compile(r'(^[A-Z]{2,}$)')
 
-    acronyms = set(filter(r.match, full_text))
+    acronyms = sorted(set(filter(r.match, full_text)))
 
-    print(acronyms)
+    filtered_acronyms = []
+    for acro in acronyms:
+        if acro not in filtered_acronyms:
+            filtered_acronyms.append(acro)
 
-    # filtered_acros = [acro for acro in acronyms if acro.lower not in words.words()]
-    # for acro in acronyms:
-    #     if acro.lower() not in set(words.words()) and acro not in set(names.words()):
-    #         filtered_acros.append(acro)
-
-    # print(filtered_acros)
+    get_definitions(filtered_acronyms, full_text)
 
 
+def get_definitions(acronyms, text):
+    stop_words = set(stopwords.words('english'))
+
+    filtered_text = []
+    for x in text:
+        if x.lower() not in stop_words:
+            filtered_text.append(x)
+
+
+    acronym_dictionary = dict()
+    # acronyms_pos = {"NN", "NNS", "NNP", "VB", "JJ", "VB", "VBD", "VBG", "VBN", "VBP"}
+
+    for acro in acronyms:
+        acronym_dictionary.fromkeys(acro, None)
+        acro_length = len(acro)
+        ngrams_lst = ngrams(text, acro_length)
+        for gram in ngrams_lst:
+            count = 0
+            for x, word in enumerate(gram):
+                if word.isalpha() and len(word) > 2 and word[0] == acro[x]:
+                    count = count+1
+            if count == acro_length:
+                if acronym_dictionary.get(acro) is not None:
+                    acronym_dictionary[acro].append(gram)
+                else:
+                    acronym_dictionary[acro] = [gram]
+
+    print(acronym_dictionary)
+
+    print("\a")
+
+    return acronym_dictionary
 
 
 if __name__ == '__main__':
-    ACL_corpus = Corpus('text data')
+    ACL_corpus = Corpus('test')
     get_acronyms(ACL_corpus)
