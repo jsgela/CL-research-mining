7642f3f8725b5b08a113d75ea7601e04a225b60e
Gela1117 <sjd.scp@gmail.com>
2018-12-19 16:10:45 -0500

    Add files; Modify corpus.py and analysis.py

--- Corpus.py
+++ Corpus.py
@@ -1,20 +1,28 @@
 #! /usr/bin/env python3
 
 import os
+from nltk.text import Text
 from nltk.corpus.reader.plaintext import PlaintextCorpusReader
 from nltk.tokenize import word_tokenize, sent_tokenize
 from nltk.corpus import stopwords
 from nltk.collocations import FreqDist
-from nltk import bigrams
-
+from nltk import bigrams, trigrams
+import json
 
 class Corpus(object):
 
     def __init__(self, data_root):
         self.data_root = data_root
         self.data = PlaintextCorpusReader(data_root, '.*')
-        self.words = self.data.words()
-        self.stop = set(stopwords.words('english'))
+        self.words = [i.lower() for i in self.data.words() if i.isalpha()]
+        self.text = Text(self.words)
+        self.stop = set(stopwords.words('english')).union(
+            {'cid','et','al','also','and','editingboston','arxiv', 'pages',
+             'trackboston','preprint','page','vol', 'volume','march','boston',
+             'table'})
+        with open('bib.json') as fi:
+            self.bib = json.load(fi)
+
 
     def documents(self):
         """Return a list of all documents in the corpus"""
@@ -49,8 +57,8 @@ class Corpus(object):
     def most_frequent_content_words(self, n_words):
         """Return a list with the most frequent content words and their
         frequencies in (word, frequency) pairs ordered by frequency"""
-        content_words = [w.lower() for w in self.words
-                         if w.lower() not in self.stop and w.isalpha()]
+        content_words = [w for w in self.words
+            if w not in self.stop and w.isalpha() and len(w)>1]
         content_words_dict = FreqDist(content_words)
         return content_words_dict.most_common(n_words)
 
@@ -59,7 +67,31 @@ class Corpus(object):
         in the form of pairs where the first element is the bigram and
         the second is its frequency"""
         bigram_dict = FreqDist([k for k in bigrams(self.words)if k[0].isalpha()
-                       and k[1].isalpha() and len(k[0])>1 and len(k[1])>1 \
-                       and k[0] not in self.stop and k[1] not in self.stop])
+            and k[1].isalpha() and len(k[0])>1 and len(k[1])>1 \
+            and k[0] not in self.stop and k[1] not in self.stop])
         return bigram_dict.most_common(n_bigrams)
+
+    def most_frequent_trigrams(self, n_trigrams):
+        trigram_dict = FreqDist([k for k in trigrams(self.words)if k[0].isalpha()
+            and k[1].isalpha() and len(k[0])>1 and len(k[1])>1 \
+            and k[0] not in self.stop and k[1] not in self.stop
+            and k[2] not in self.stop])
+        return trigram_dict.most_common(n_trigrams)
+
+    def get_info(self, fileID):
+        """Return metadata associate with a file indexed by the following fields:
+        author, title, booktitle, year, publisher, pages, location, doi, url"""
+        return self.bib[fileID]
+
+    def print_reference(self, fileID):
+        """Print metadata (author, title of paper, title of book, publishing year)
+        associated with each file as a reference"""
+        d = self.bib[fileID]
+        print(
+            "%s. %s. %s, %s" %
+            (' '.join(d['author'].split('\n')), d['title'], d['booktitle'], d['year'])
+        )
+
+    def concordance(self, word):
+        self.text.concordance(word)
 
