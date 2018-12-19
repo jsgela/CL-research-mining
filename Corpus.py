6d81398840bb2cfb6014afde9b4ffd423f3753fd
Gela1117 <sjd.scp@gmail.com>
2018-12-19 17:41:14 -0500

    Fix

--- Corpus.py
+++ Corpus.py
@@ -26,7 +26,7 @@ class Corpus(object):
 
     def documents(self):
         """Return a list of all documents in the corpus"""
-	return sorted([i for i in os.listdir(self.data_root)])
+        return sorted([i for i in os.listdir(self.data_root)])
 
     def words_in_file(self, filename):
         """Given a file, return a list of tokenized words"""
@@ -45,16 +45,15 @@ class Corpus(object):
         return sent_tokenize(text)
 
     def tokenized_sentences_in_file(self, filename):
-	"""Given a file name, return a list of word tokenized sentences"""
+        """Given a file, return a list of sentences
+         in which each sentence is a list of tokens"""
         try:
             text = self.data.open(filename).read()
-            sent = []
-            for s in sent_tokenize(text):
-                sent.append(word_tokenize(s))
+            sent = [word_tokenize(s) for s in sent_tokenize(text)]
         except FileNotFoundError:
             print("The file does not exist.")
         return sent
-    
+
     def most_frequent_content_words(self, n_words):
         """Return a list with the most frequent content words and their
         frequencies in (word, frequency) pairs ordered by frequency"""
@@ -64,7 +63,7 @@ class Corpus(object):
         return content_words_dict.most_common(n_words)
 
     def most_frequent_bigrams(self, n_bigrams):
-	"""Return a list with the most frequent bigrams of content words
+        """Return a list with the most frequent bigrams of content words
         in the form of pairs where the first element is the bigram and
         the second is its frequency"""
         bigram_dict = FreqDist([k for k in bigrams(self.words)if k[0].isalpha()
@@ -95,3 +94,4 @@ class Corpus(object):
 
     def concordance(self, word):
         self.text.concordance(word)
+
