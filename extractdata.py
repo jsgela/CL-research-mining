b146c7362eddfa7e72645b94395ebcd4e91a1341
Gela1117 <sjd.scp@gmail.com>
2018-12-19 17:46:31 -0500

    Update

--- extractdata.py
+++ extractdata.py
@@ -5,6 +5,10 @@ from pdfminer.layout import LAParams
 from pdfminer.pdfpage import PDFPage
 import os
 import sys, getopt
+import nltk.tokenize
+import nltk
+from pathlib import Path
+import codecs
 
 #converts pdf, returns its text content as a string
 def convert(fname, pages=None):
@@ -39,6 +43,21 @@ def convertMultiple(pdfDir, txtDir):
             textFile.write(text) #write text to text file
             #textFile.close
 
-pdfDir = "C:/Users/86136/Desktop/HW/IntroNLP/Final Project/Data/Papers/"
+pdfDir = "C:/Users/86136/Desktop/HW/IntroNLP/Final Project/con27/pdf/"
 txtDir = "C:/Users/86136/Desktop/HW/IntroNLP/Final Project/Data/Papers_txt/"
 convertMultiple(pdfDir, txtDir)
+
+# pick out those without space
+def pickout(path):
+     file_lst = [f.name for f in Path(path).iterdir() if f.is_file()]
+     for filename in file_lst:
+        full_path = path+filename
+        with codecs.open(full_path, 'r', encoding="utf-8") as f:
+            r = f.read()
+            w_lst= nltk.word_tokenize(r)
+            for w in w_lst:
+                if len(w) >= 100:
+                    f.close()
+                    os.remove(full_path)
+                    break
+pickout(txtDir)
