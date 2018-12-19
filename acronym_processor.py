e4c0043db32c58fa3dc4cabb5a55a149d566e0b9
Di5to <di5tortion@yahoo.com>
2018-12-19 17:23:02 -0500

    Added extra definitions filter

--- acronym_processor.py
+++ acronym_processor.py
@@ -7,24 +7,11 @@ import time
 
 def get_acronyms(ACL_corpus):
 
-    #create a full list of tokenized words from the corpus
-#    full_text = []
-#    for thing in ACL_corpus.documents():
-#        for lsts in ACL_corpus.tokenized_sentences_in_file(thing):
-#            full_text.extend(lsts)
-
-    #
-    # filtered_text = []
-    # for x in full_text:
-    #     if x.lower() not in names.words():
-    #         filtered_text.append(x)
-
-    print("done filtering text")
-
-    r = re.compile(r'(^[A-Z]{2,}$)')
-
+    # r = re.compile(r'(^[A-Z]{2,}$)')
     #acronyms = sorted(set(filter(r.match, full_text)))
+
     full_text = ACL_corpus.words
+
     acronyms = {elm for elm in full_text if elm.isupper() and elm.isalpha() and len(elm) > 1}
 #    print(acronyms)
     return get_definitions(list(acronyms), full_text)
@@ -38,12 +25,7 @@ def get_definitions(acronyms, text):
         if x.lower() not in stop_words:
             filtered_text.append(x)
 
-
-    # acronym_dictionary = dict()
-    # acronyms_pos = {"NN", "NNS", "NNP", "VB", "JJ", "VB", "VBD", "VBG", "VBN", "VBP"}
-    
-    # print(len(acronyms))
-    # acronyms = acronyms[:5]
+    # acronyms = acronyms[:100]
     acronym_dictionary = {}
 
     a = time.time()
@@ -57,55 +39,20 @@ def get_definitions(acronyms, text):
         for i in range(len(filtered_text) - acro_len):
             chosen = True
             for j in range(acro_len):
-                if not filtered_text[i+j][0] == acro[j]:
-#                if len(filtered_text[i+j]) < 2 or not filtered_text[i+j][0] == acro[j]:
+               if len(filtered_text[i+j]) < 2 or not filtered_text[i+j][0] == acro[j] or filtered_text[i+j].isupper():
                     chosen = False
                     break
             if chosen:
                 word = ' '.join(filtered_text[i:i+acro_len])
                 matches.add(word)
-                #matches.append(word)
         if not len(matches) == 0:
             acronym_dictionary[acro] = list(set(matches))
-#    for acro in acronyms:
-#        acronym_dictionary.fromkeys(acro, None)
-#        acro_length = len(acro)
-#        i = 0
-#        while i < len(filtered_text)-acro_length:
-#            chosen_words = filtered_text[i:i+acro_length]
-#            # print("acronym = ", acro, " : ", chosen_words)
-#            count = 0
-#            for x, word in enumerate(chosen_words):
-#                if word.isalpha() and len(word) > 2 and word[0] == acro[x]:
-#                    count += 1
-#            if count == acro_length:
-#                # print(chosen_words)
-#                if acronym_dictionary.get(acro) is not None:
-#                    acronym_dictionary[acro].add(' '.join(chosen_words))
-#                else:
-#                    acronym_dictionary[acro] = {' '.join(chosen_words)}
-#            i += 1
 
-        # ngrams_set = set(ngrams(text, acro_length))
-        # for gram in ngrams_set:
-        #     gram_count = gram_count+1
-        #     count = 0
-        #     for x, word in enumerate(gram):
-        #         if word.isalpha() and len(word) > 2 and word[0] == acro[x]:
-        #             count = count+1
-        #     if count == acro_length:
-        #         if acronym_dictionary.get(acro) is not None:
-        #             acronym_dictionary[acro].append(gram)
-        #         else:
-        #             acronym_dictionary[acro] = [gram]
-        #         print(acro)
-        #         print(gram)
 
     b = time.time()
 #    print(len(acronym_dictionary))
 #    print(acronym_dictionary)
-#    print("Time", b - a)
-#    print('\a')
+    print("Time", b - a)
 
     return acronym_dictionary
 
