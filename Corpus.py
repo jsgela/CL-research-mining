34c1e21bbf8f697b7568d45bbfdeea6854d778e7
Kuan-Yu Chen <chenky@brandeis.edu>
2018-12-19 20:39:47 -0800

    Update Corpus.py

--- Corpus.py
+++ Corpus.py
@@ -14,7 +14,7 @@ class Corpus(object):
     def __init__(self, data_root):
         self.data_root = data_root
         self.data = PlaintextCorpusReader(data_root, '.*')
-        self.words = [i.lower() for i in self.data.words() if i.isalpha()]
+        self.words = [i for i in self.data.words() if i.isalpha()]
         self.text = Text(self.words)
         self.stop = set(stopwords.words('english')).union(
             {'cid','et','al','also','and','editingboston','arxiv', 'pages',
@@ -27,6 +27,7 @@ class Corpus(object):
     def documents(self):
         """Return a list of all documents in the corpus"""
         return sorted([i for i in os.listdir(self.data_root)])
+
 
     def words_in_file(self, filename):
         """Given a file, return a list of tokenized words"""
@@ -57,7 +58,7 @@ class Corpus(object):
         """Return a list with the most frequent content words and their
         frequencies in (word, frequency) pairs ordered by frequency"""
         content_words = [w for w in self.words
-            if w not in self.stop and w.isalpha() and len(w)>1]
+            if w.lower() not in self.stop and w.isalpha() and len(w)>1]
         content_words_dict = FreqDist(content_words)
         return content_words_dict.most_common(n_words)
 
@@ -67,14 +68,14 @@ class Corpus(object):
         the second is its frequency"""
         bigram_dict = FreqDist([k for k in bigrams(self.words)if k[0].isalpha()
             and k[1].isalpha() and len(k[0])>1 and len(k[1])>1 \
-            and k[0] not in self.stop and k[1] not in self.stop])
+            and k[0].lower() not in self.stop and k[1].lower() not in self.stop])
         return bigram_dict.most_common(n_bigrams)
 
     def most_frequent_trigrams(self, n_trigrams):
         trigram_dict = FreqDist([k for k in trigrams(self.words)if k[0].isalpha()
             and k[1].isalpha() and len(k[0])>1 and len(k[1])>1 \
-            and k[0] not in self.stop and k[1] not in self.stop
-            and k[2] not in self.stop])
+            and k[0].lower() not in self.stop and k[1].lower() not in self.stop
+            and k[2].lower() not in self.stop])
         return trigram_dict.most_common(n_trigrams)
 
     def get_info(self, fileID):
